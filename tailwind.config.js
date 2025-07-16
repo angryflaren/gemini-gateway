6cacc77d99366e040e05485795db03ac553efcec
Matthew Zhuchkov <136967840+angryflaren@users.noreply.github.com>
2025-07-16 23:23:21 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
-/** @type {import('tailwindcss').Config} */
+import typography from "@tailwindcss/typography";
+
+/** @type {import("tailwindcss").Config} */
 export default {
-  darkMode: 'class',
+  darkMode: "class",
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -8,5 +10,7 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    typography,
+  ],
 }
