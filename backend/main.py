8b01217ee99037a8e745810c986860251f8c0661
Matthew Zhuchkov <136967840+angryflaren@users.noreply.github.com>
2025-07-30 14:32:45 +0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,7 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://angryflaren.github.io",
+    ""https://angryflaren.github.io/gemini-gateway/" ",
 ]
 
 
